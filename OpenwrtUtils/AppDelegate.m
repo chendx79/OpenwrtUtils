0e92baa4e943da92be8fb9019f2537a1cf6e172a
gao <gao@coconut.local>
2017-03-14 16:34:09 +0800

    协同开发，代码提交测试

--- OpenwrtUtils/AppDelegate.m
+++ OpenwrtUtils/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    // github push test
     return YES;
 }
 
