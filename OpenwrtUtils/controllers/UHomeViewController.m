6055312e614f1d74f74eb6d543319d2e08ab5d8f
chend79 <chendx79@hotmail.com>
2017-05-05 10:50:17 +0800

    auto login using stored password

--- OpenwrtUtils/controllers/UHomeViewController.m
+++ OpenwrtUtils/controllers/UHomeViewController.m
@@ -11,9 +11,9 @@
 #import "URouterConfig.h"
 
 #define IMAGE_INTERNET [UIImage imageNamed:@"internet"]
-#define IMAGE_ROUTER   [UIImage imageNamed:@"access_point"]
-#define IMAGE_SEARCH   [UIImage imageNamed:@"search_wifi"]
-#define IMAGE_BOX      [UIImage imageNamed:@"openwrtbox"]
+#define IMAGE_ROUTER [UIImage imageNamed:@"access_point"]
+#define IMAGE_SEARCH [UIImage imageNamed:@"search_wifi"]
+#define IMAGE_BOX [UIImage imageNamed:@"openwrtbox"]
 
 /**
  逻辑
@@ -24,7 +24,6 @@
  5. 密码需要缓存下来，下次连上盒子会直接登录，走步骤4流程
  6. 点击WiFi图标，跳转到WiFi搜索列表，在列表中连接成功后，WiFi图标改成路由器图标
  */
-
 
 @interface UHomeViewController ()
 @property (nonatomic, strong) UIButton *internetButton;
@@ -42,73 +41,83 @@
     // Do any additional setup after loading the view.
     self.navigationItem.title = @"Openwrt实用工具";
     NSLog(@"CheckUBus started...");
-    
+
     [self.view addSubview:self.internetButton];
     [self.view addSubview:self.wifiButton];
     [self.view addSubview:self.boxButton];
-    
+
     [self.internetButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerX.mas_equalTo(self.view.mas_centerX);
-        make.top.mas_equalTo(80);
-        make.width.mas_equalTo(100);
-        make.height.mas_equalTo(100);
+      make.centerX.mas_equalTo(self.view.mas_centerX);
+      make.top.mas_equalTo(80);
+      make.width.mas_equalTo(100);
+      make.height.mas_equalTo(100);
     }];
-    
+
     [self.wifiButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerX.mas_equalTo(self.internetButton.mas_centerX);
-        make.top.mas_equalTo(self.internetButton.mas_bottom).offset(40);
-        make.width.mas_equalTo(100);
-        make.height.mas_equalTo(100);
+      make.centerX.mas_equalTo(self.internetButton.mas_centerX);
+      make.top.mas_equalTo(self.internetButton.mas_bottom).offset(40);
+      make.width.mas_equalTo(100);
+      make.height.mas_equalTo(100);
     }];
-    
+
     [self.boxButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerX.mas_equalTo(self.internetButton.mas_centerX);
-        make.top.mas_equalTo(self.wifiButton.mas_bottom).offset(40);
-        make.width.mas_equalTo(100);
-        make.height.mas_equalTo(100);
+      make.centerX.mas_equalTo(self.internetButton.mas_centerX);
+      make.top.mas_equalTo(self.wifiButton.mas_bottom).offset(40);
+      make.width.mas_equalTo(100);
+      make.height.mas_equalTo(100);
     }];
-    
+
     self.wifiButton.hidden = YES;
     self.boxButton.hidden = YES;
-    
-    [[RACSignal combineLatest:@[RACObserve([URouterConfig sharedInstance], isBoxAvailable)] reduce:^(NSNumber *isBoxAvailable) {
-        return @(isBoxAvailable.boolValue);
-    }] subscribeNext:^(NSNumber *rst) {
-        if (rst.boolValue) {
-            self.boxButton.hidden = NO;
-        }
-        else {
-            self.boxButton.hidden = YES;
-        }
+
+    [[RACSignal combineLatest:@[ RACObserve([URouterConfig sharedInstance], isBoxAvailable) ]
+                       reduce:^(NSNumber *isBoxAvailable) {
+                         return @(isBoxAvailable.boolValue);
+                       }] subscribeNext:^(NSNumber *rst) {
+      if (rst.boolValue) {
+          self.boxButton.hidden = NO;
+      } else {
+          self.boxButton.hidden = YES;
+      }
     }];
-    
-    [[RACSignal combineLatest:@[RACObserve([URouterConfig sharedInstance], isBoxLoggedin)] reduce:^(NSNumber *isBoxLoggedin) {
-        return @(isBoxLoggedin.boolValue);
-    }] subscribeNext:^(NSNumber *rst) {
-        if (rst.boolValue) {
-            self.wifiButton.hidden = NO;
-        }
-        else {
-            self.wifiButton.hidden = YES;
-        }
+
+    [[RACSignal combineLatest:@[ RACObserve([URouterConfig sharedInstance], isBoxLoggedin) ]
+                       reduce:^(NSNumber *isBoxLoggedin) {
+                         return @(isBoxLoggedin.boolValue);
+                       }] subscribeNext:^(NSNumber *rst) {
+      if (rst.boolValue) {
+          self.wifiButton.hidden = NO;
+      } else {
+          self.wifiButton.hidden = YES;
+      }
     }];
-    
-    [[RACSignal combineLatest:@[RACObserve([URouterConfig sharedInstance], isWiFiConnected)] reduce:^(NSNumber *isWiFiConnected) {
-        return @(isWiFiConnected.boolValue);
-    }] subscribeNext:^(NSNumber *rst) {
-        if (rst.boolValue) {
-            [self.wifiButton setBackgroundImage:IMAGE_ROUTER forState:UIControlStateNormal];
-        }
-        else {
-            [self.wifiButton setBackgroundImage:IMAGE_SEARCH forState:UIControlStateNormal];
-        }
+
+    [[RACSignal combineLatest:@[ RACObserve([URouterConfig sharedInstance], isWiFiConnected) ]
+                       reduce:^(NSNumber *isWiFiConnected) {
+                         return @(isWiFiConnected.boolValue);
+                       }] subscribeNext:^(NSNumber *rst) {
+      if (rst.boolValue) {
+          [self.wifiButton setBackgroundImage:IMAGE_ROUTER forState:UIControlStateNormal];
+      } else {
+          [self.wifiButton setBackgroundImage:IMAGE_SEARCH forState:UIControlStateNormal];
+      }
     }];
-    
+
     [self actionInternet:nil];
+    
+    //启动时自动用之前存储的密码来尝试登陆路由器
+    NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"Config" ofType:@"plist"];
+    NSMutableDictionary *data = [[NSMutableDictionary alloc] initWithContentsOfFile:plistPath];
+    NSString *rootPassword = [data objectForKey:@"rootPassword"];
+    
+    [[URouterConfig sharedInstance] loginWithPassword:rootPassword
+                                               result:^(BOOL success) {
+                                                   [self hideLoading];
+                                               }];
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
+    [super didReceiveMemoryWarning];OpenwrtUtils/controllers/UHomeViewController.m
     // Dispose of any resources that can be recreated.
 }
 
@@ -150,15 +159,14 @@
 - (void)actionInternet:(id)sender {
     [self showLoading];
     [[URouterConfig sharedInstance] checkBoxAvailable:^(BOOL available) {
-        [self hideLoading];
-        NSString *message = nil;
-        if (available) {
-            message = @"路由器支持UBus访问";
-        }
-        else {
-            message = @"路由器不支持UBus访问";
-        }
-        [self toast:message];
+      [self hideLoading];
+      NSString *message = nil;
+      if (available) {
+          message = @"路由器支持UBus访问";
+      } else {
+          message = @"路由器不支持UBus访问";
+      }
+      [self toast:message];
     }];
 }
 
@@ -177,9 +185,10 @@
     if (buttonIndex == alertView.firstOtherButtonIndex) {
         UITextField *pwdField = [alertView textFieldAtIndex:0];
         [self showLoading];
-        [[URouterConfig sharedInstance] loginWithPassword:pwdField.text result:^(BOOL success) {
-            [self hideLoading];
-        }];
+        [[URouterConfig sharedInstance] loginWithPassword:pwdField.text
+                                                   result:^(BOOL success) {
+                                                     [self hideLoading];
+                                                   }];
     }
 }
 
