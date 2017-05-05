e3284867297ce3d537c0f57582ee59f47a12cf02
chend79 <chendx79@hotmail.com>
2017-05-05 15:30:24 +0800

    add about vc

---
+++ OpenwrtUtils/controllers/UAboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  UAboutViewController.h
+//  OpenwrtUtils
+//
+//  Created by lujingyu on 2017/5/5.
+//  Copyright © 2017年 陈鼎星. All rights reserved.
+//
+
+#import "UBaseViewController.h"
+
+@interface UAboutViewController : UBaseViewController
+
+@end
--- OpenwrtUtils/controllers/UAboutViewController.m
+++ OpenwrtUtils/controllers/UAboutViewController.m
@@ -1,28 +1,27 @@
 //
-//  UBoxViewController.m
+//  UAboutViewController.m
 //  OpenwrtUtils
 //
 //  Created by lujingyu on 2017/3/17.
 //  Copyright © 2017年 陈鼎星. All rights reserved.
 //
 
-#import "UBoxViewController.h"
+#import "UAboutViewController.h"
 #import "UWiFiPwdViewController.h"
 #import "URouterConfig.h"
 #import "UWiFiSearchResultCell.h"
-#import "UAboutViewController.h"
 
-@interface UBoxViewController () <UITableViewDataSource, UITableViewDelegate>
+@interface UAboutViewController () <UITableViewDataSource, UITableViewDelegate>
 @property (nonatomic, strong) UITableView *tableView;
 @property (nonatomic, strong) NSMutableArray *datasource;
 @end
 
-@implementation UBoxViewController
+@implementation UAboutViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.navigationItem.title = @"Openwrt路由器";
+    self.navigationItem.title = @"关于本机";
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"返回"
                                                                              style:UIBarButtonItemStylePlain
                                                                             target:self
@@ -46,27 +45,15 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     // Return the number of sections.
-    return 3;
+    return 2;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    if (section == 1) {
-        return @"网络";
-    }
-    if (section == 2) {
-        return @"服务";
-    }
     return @"";
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    if (indexPath.section == 0) {
-        if (indexPath.row == 0){
-            UAboutViewController *vc = [[UAboutViewController alloc] init];
-            [self.navigationController pushViewController:vc animated:YES];
-        }
-    }
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -74,10 +61,7 @@
         return 1;
     }
     if (section == 1) {
-        return 3;
-    }
-    if (section == 2) {
-        return 2;
+        return 5;
     }
     return 0;
 }
@@ -91,42 +75,40 @@
     if (indexPath.section == 0) {
         if (indexPath.row == 0) {
             UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(30, 11, 150, 21)];
-            label.text = @"关于本机";
-            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            label.text = @"主机名";
+            cell.accessoryType = UITableViewCellAccessoryNone;
             [cell.contentView addSubview:label];
         }
     }
     if (indexPath.section == 1) {
         if (indexPath.row == 0) {
             UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(30, 11, 150, 21)];
-            label.text = @"公网";
-            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            label.text = @"设备型号";
+            cell.accessoryType = UITableViewCellAccessoryNone;
             [cell.contentView addSubview:label];
         }
         if (indexPath.row == 1) {
             UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(30, 11, 150, 21)];
-            label.text = @"局域网";
-            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            label.text = @"固件版本";
+            cell.accessoryType = UITableViewCellAccessoryNone;
             [cell.contentView addSubview:label];
         }
         if (indexPath.row == 2) {
             UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(30, 11, 150, 21)];
-            label.text = @"无线网络";
-            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            label.text = @"运行时间";
+            cell.accessoryType = UITableViewCellAccessoryNone;
             [cell.contentView addSubview:label];
         }
-    }
-    if (indexPath.section == 2) {
-        if (indexPath.row == 0) {
+        if (indexPath.row == 3) {
             UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(30, 11, 150, 21)];
-            label.text = @"Shadowsocks";
-            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            label.text = @"总容量";
+            cell.accessoryType = UITableViewCellAccessoryNone;
             [cell.contentView addSubview:label];
         }
-        if (indexPath.row == 1) {
+        if (indexPath.row == 4) {
             UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(30, 11, 150, 21)];
-            label.text = @"Pdnsd";
-            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            label.text = @"可用容量";
+            cell.accessoryType = UITableViewCellAccessoryNone;
             [cell.contentView addSubview:label];
         }
     }
